a83acf06b9a0ba5979fd73a10e5f0608bf40ddc6
Mahmoud Eldengawy <eldengaawy@gmail.com>
2023-08-22 23:59:27 +0300

    App Colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF52DCE4);
+  static const Color backgroundColor = Color(0xFFDCF8F6);
+  static const Color textColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+  static const Color dividerColor = Colors.grey;
+  static Color timeColor = Colors.grey[600]!;
+  static Color shadowColor = Colors.black.withOpacity(0.1);
+}
